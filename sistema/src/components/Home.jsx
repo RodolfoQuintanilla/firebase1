b88ff8446352a7b550428b42cc4aa368dc44d196
RodolfoQuintanilla <rqm@outlook.es>
2022-10-06 22:04:22 -0500

    fin video 10

--- sistema/src/components/Home.jsx
+++ sistema/src/components/Home.jsx
@@ -1,92 +1,139 @@
-import React, { useState } from 'react';
-import appfirebase from '../credenciales';
-import { getAuth, signOut } from 'firebase/auth';
-import { getFirestore, collection,addDoc, getDocs, doc,deleteDoc, getDoc, setDoc } from "firebase/firestore";
+import React, { useEffect, useState } from 'react'
+import appfirebase from '../credenciales'
+import { getAuth, signOut } from 'firebase/auth'
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  deleteDoc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore'
 
-const auth = getAuth(appfirebase);
-const db = getFirestore(appfirebase);
-
+const auth = getAuth(appfirebase)
+const db = getFirestore(appfirebase)
 
 const Home = ({ correoUsuario }) => {
-   console.log(correoUsuario);
+  console.log(correoUsuario)
 
-   const valorInicial = {
-      nombre: '',
-      edad: '',
-      profecion: ''
-   }
+  const valorInicial = {
+    nombre: '',
+    edad: '',
+    profecion: '',
+  }
 
+  const [usuario, setUsuario] = useState(valorInicial)
+  const [lista, setLista] = useState([])
 
-   const [usuario, setUsuario] = useState(valorInicial);
+  const capturarInputs = (e) => {
+    const { name, value } = e.target
+    setUsuario({ ...usuario, [name]: value })
+  }
 
-   const capturarInputs = (e) => {
-      const { name, value } = e.target;
-      setUsuario({ ...usuario, [name]: value })
-   }
-
-   const guardarDatos = async (e) => {
-      e.preventDefault();
-     /* console.log(usuario); */
-     try {
-      await addDoc(collection(db,'usuarios'),{
-         ...usuario
+  const guardarDatos = async (e) => {
+    e.preventDefault()
+    /* console.log(usuario); */
+    try {
+      await addDoc(collection(db, 'usuarios'), {
+        ...usuario,
       })
-     } catch (error) {
-      console.log(error);
-     }
-      setUsuario({...valorInicial})
-   }
+    } catch (error) {
+      console.log(error)
+    }
+    setUsuario({ ...valorInicial })
+  }
 
-   return (
-      <div className='container'>
-         <p>Bienvenido <strong>{correoUsuario}</strong> Haz Iniciado Secion </p>
+  useEffect(() => {
+    const getlista = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'usuarios'))
+        const docs = []
+        querySnapshot.forEach((doc) => {
+          docs.push({ ...doc.data(), id: doc.id })
+        })
+        setLista(docs)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getlista()
+  }, [lista])
 
-         <button className='btn btn-primary' onClick={() => signOut(auth)}>
-            Cerra Secion
-         </button>
-         <hr />
+  return (
+    <div className="container">
+      <p>
+        Bienvenido <strong>{correoUsuario}</strong> Haz Iniciado Secion{' '}
+      </p>
 
-         <div className='row'>
-            {/* Esta seccion es del Formulartio */}
-            <div className="col-md-4 ">
-               <h3 className='text-center mb-3'>Ingresar Usuarios</h3>
-               <form onSubmit={guardarDatos}>
-                  <div className="card card-body">
-                     <div className="from-group">
-                        <input type="text"
-                           name='nombre' className='form-control mb-3'
-                           placeholder='Ingresar El Nombre del Usuario'
-                           onChange={capturarInputs}
-                           value={usuario.nombre}
-                        />
-                        <input type="text" name='edad' className='form-control mb-3'
-                           placeholder='Ingresar la Edad'
-                           onChange={capturarInputs}
-                           value={usuario.edad}
-                        />
+      <button
+        className="btn btn-primary botonMio"
+        onClick={() => signOut(auth)}
+      >
+        Cerra Secion
+      </button>
+      <hr />
 
-                        <input type="text" name='profecion'
-                           className='form-control mb-3'
-                           placeholder='Ingresar la Profecion'
-                           onChange={capturarInputs}
-                           value={usuario.profecion}
-                        />
+      <div className="row">
+        {/* Esta seccion es del Formulartio */}
+        <div className="col-md-4 ">
+          <h3 className="text-center mb-3">Ingresar Usuarios</h3>
+          <form onSubmit={guardarDatos}>
+            <div className="card card-body">
+              <div className="from-group">
+                <input
+                  type="text"
+                  name="nombre"
+                  className="form-control mb-3"
+                  placeholder="Ingresar El Nombre del Usuario"
+                  onChange={capturarInputs}
+                  value={usuario.nombre}
+                />
+                <input
+                  type="text"
+                  name="edad"
+                  className="form-control mb-3"
+                  placeholder="Ingresar la Edad"
+                  onChange={capturarInputs}
+                  value={usuario.edad}
+                />
 
-                     </div>
-                     <button className='btn btn-primary'>
-                        Guardar
-                     </button>
-                  </div>
-               </form>
+                <input
+                  type="text"
+                  name="profecion"
+                  className="form-control mb-3"
+                  placeholder="Ingresar la Profecion"
+                  onChange={capturarInputs}
+                  value={usuario.profecion}
+                />
+              </div>
+              <button className="btn btn-primary">Guardar</button>
             </div>
-            {/*  Esta seccion lista de usuarios */}
-            <div className="col-md-8">
-               <h2 className='text-center'>Lista de Usuarios</h2>
-            </div>
+          </form>
+        </div>
+        {/*  Esta seccion lista de usuarios */}
+        <div className="col-md-8">
+          <h2 className="text-center">Lista de Usuarios</h2>
 
-         </div>
+          <div className="container card">
+            <div className="card-body">
+              {lista.map((list) => (
+                <div key={list.id}>
+                  <p>Nombre: {list.nombre}</p>
+                  <p>Edad: {list.edad} </p>
+                  <p>profecion: {list.profecion}</p>
+                  <button className="btn btn-success">Editar</button>
+                  <button className="btn btn-danger">Eliminar</button>
+                  <hr />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-   );
-};
+    </div>
+  )
+}
 
-export default Home; 
+export default Home
